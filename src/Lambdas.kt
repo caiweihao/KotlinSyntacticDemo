9a47522dc0adfc865742e64c3cf22a4627200d3b
whcai <whcai@mobvoi.com>
2018-04-18 17:28:14 +0800

    anonymous function vs lambda in high order function invoke

--- src/Lambdas.kt
+++ src/Lambdas.kt
@@ -1,13 +1,11 @@
 package lambdas
 
-
 fun buildString(
         builderAction: (StringBuilder) -> Unit): String {
     val sb = StringBuilder()
     builderAction(sb)
     return sb.toString()
 }
-
 
 
 fun buildStringWithResult(
@@ -50,8 +48,13 @@ fun main(args: Array<String>) {
     println("sum fun result is ${numFun(1, 3)}")
     println("sumLambda fun result is ${sumLambda(1, 3)}")
 
+    println("lambda invoke is ${op(3, {it * it})}")
+    println("anonymous functions invoke is ${op(11, fun(x): Int { return if (x > 10) 0 else x * x })}")
 
+}
 
+fun op(x: Int, op: (Int) -> Int): Int {
+    return op(x)
 }
 
 fun sum(a: Int, b: Int): Int {
